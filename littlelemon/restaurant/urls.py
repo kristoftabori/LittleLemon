03caa836e4ac0f52966f51dbfa1303d1529efe1e
kristoftabori <tunderherceg@gmail.com>
2023-10-18 13:17:39 +0200

    API basics added

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
   
 urlpatterns = [ 
-    path('', views.index, name='index')
+    # path('', views.index, name='index'),
+    path('menu/', views.MenuView.as_view()),
+    path('booking/', views.BookingView.as_view()),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -2,5 +2,38 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-def index(request):
-    return render(request, 'index.html', {})
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer
+
+
+class BookingView(APIView):
+
+    def get(self, request):
+        items = Booking.objects.all()
+        serializer = BookingSerializer(items, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = BookingSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data})
+
+
+class MenuView(APIView):
+
+    def get(self, request):
+        items = Menu.objects.all()
+        serializer = MenuSerializer(items, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = MenuSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data})

d23799dc93b806b64324d2a4c42a596e74a2e14a
kristoftabori <tunderherceg@gmail.com>
2023-10-18 15:35:43 +0200

    install djoser

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'restaurant',
     'rest_framework',
     'rest_framework.authtoken',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -142,5 +143,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ]
+}
+
+DJOSER = {
+    "USER_ID_FIELD": "username",
 }
